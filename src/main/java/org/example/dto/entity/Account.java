9aa5812f15ca4c2c90cb555de5fcfac0103f0879
Sheddy <hei.sheddy.2@gmail.com>
2024-11-14 03:56:18 +0300

    feat: Getter, Setter && Constructeur for Account

--- src/main/java/org/example/dto/entity/Account.java
+++ src/main/java/org/example/dto/entity/Account.java
@@ -13,4 +13,57 @@ public class Account {
     public enum Type {
         Banque, OrangeMoney, Espèce
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public double getDevise() {
+        return devise;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setDevise(double devise) {
+        this.devise = devise;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Account(String id, String name, double balance, List<Transaction> transactions, double devise, Type type) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.transactions = transactions;
+        this.devise = devise;
+        this.type = type;
+    }
 }
