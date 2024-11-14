50a2869f4a41423c9d026b8a3e543e93e29e3a8c
Sheddy <hei.sheddy.2@gmail.com>
2024-11-14 03:57:25 +0300

    feat: Getter, Setter && Constructeur for Devise

--- src/main/java/org/example/dto/entity/Devise.java
+++ src/main/java/org/example/dto/entity/Devise.java
@@ -8,4 +8,30 @@ public class Devise {
     public enum Name{
         Euro, Ariary
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Devise(String id, Name name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
 }
