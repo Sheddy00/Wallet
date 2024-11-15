bbbd290ff4f0e0bfd7c621380682a20fa0fe42b4
Sheddy <hei.sheddy.2@gmail.com>
2024-11-15 19:14:18 +0300

    feat: Implements CRUDOperation for Devise

--- src/main/java/org/example/operation/DeviseOperation.java
+++ src/main/java/org/example/operation/DeviseOperation.java
@@ -1,4 +1,37 @@
 package org.example.operation;
 
-public class DeviseOperation {
+import org.example.dto.entity.Devise;
+
+import java.util.List;
+
+public class DeviseOperation implements CRUDOperation<Devise> {
+    @Override
+    public List<Devise> findAll() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Devise findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Devise> saveAll(List<Devise> entities) {
+        return List.of();
+    }
+
+    @Override
+    public Devise save(Devise devise) {
+        return null;
+    }
+
+    @Override
+    public Devise update(Devise devise) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
