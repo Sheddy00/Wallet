939ebb7a9f48e2286bfdb7f742beca2bb40dc25f
Sheddy <hei.sheddy.2@gmail.com>
2024-11-15 19:13:38 +0300

    feat: Implements CRUDOperation for Account

--- src/main/java/org/example/operation/AccountOperation.java
+++ src/main/java/org/example/operation/AccountOperation.java
@@ -1,4 +1,37 @@
 package org.example.operation;
 
-public class AccountOperation {
+import org.example.dto.entity.Account;
+
+import java.util.List;
+
+public class AccountOperation implements CRUDOperation<Account> {
+    @Override
+    public List<Account> findAll() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Account findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Account> saveAll(List<Account> entities) {
+        return List.of();
+    }
+
+    @Override
+    public Account save(Account account) {
+        return null;
+    }
+
+    @Override
+    public Account update(Account account) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
