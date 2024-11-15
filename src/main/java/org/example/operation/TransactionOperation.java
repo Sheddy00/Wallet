dad513b175a7f0d4695b56e049ee76688ef533f0
Sheddy <hei.sheddy.2@gmail.com>
2024-11-15 19:14:53 +0300

    feat: Implements CRUDOperation for Transaction

--- src/main/java/org/example/operation/TransactionOperation.java
+++ src/main/java/org/example/operation/TransactionOperation.java
@@ -1,4 +1,37 @@
 package org.example.operation;
 
-public class TransactionOperation {
+import org.example.dto.entity.Transaction;
+
+import java.util.List;
+
+public class TransactionOperation implements CRUDOperation<Transaction> {
+    @Override
+    public List<Transaction> findAll() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Transaction findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> saveAll(List<Transaction> entities) {
+        return List.of();
+    }
+
+    @Override
+    public Transaction save(Transaction transaction) {
+        return null;
+    }
+
+    @Override
+    public Transaction update(Transaction transaction) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
